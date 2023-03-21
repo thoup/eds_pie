e707c073ef48a5590c2dccab53de91216b9b303e
Singh <ekaum.singh@molex.com>
2023-03-21 12:43:16 -0400

    Create package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='eds_pie',
+    version='1',
+    description='EDS files',
+    url='https://github.com/thoup/eds_pie.git',
+    author='thoup',
+    author_email='NA',
+    license='MIT license',
+    packages=['eds_pie'],
+    zip_safe=False
+)
